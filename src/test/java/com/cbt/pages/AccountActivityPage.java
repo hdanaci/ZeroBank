2b567ebc4ff25cf6f60964fd4a2bfffc0ed627e4
sulederekoy <edanildy@gmail.com>
2018-08-04 17:43:54 -0500

    add account_a_Page

--- src/test/java/com/cbt/pages/AccountActivityPage.java
+++ src/test/java/com/cbt/pages/AccountActivityPage.java
@@ -14,10 +14,10 @@ public class AccountActivityPage {
 		PageFactory.initElements(driver, this);
 	}		
 	
-	@FindBy(xpath="//li[@id=\"account_activity_tab\"]")
+	@FindBy(xpath="//*[@class='active']//*[text()='Account Activity']")
 	public WebElement accountActivityTab;
 	
-	@FindBy(className="board-header")
+	@FindBy(xpath="//h2[@class='board-header']")
 	public WebElement showTransactions;
 
 	@FindBy(xpath="//option[1]")
